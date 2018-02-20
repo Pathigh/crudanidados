12618dde05a076fb06717d7d8b808c26ca02b822
Patricio <pat.venegas.l@gmail.com>
2018-02-20 20:09:40 -0300

    primer comentario

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,39 @@
+class ProductsController < ApplicationController
+
+before_action :set_category, only: [:create, :destroy]
+
+	def create
+		@product = Product.new(product_params)
+		@product.category = Category.find(params[:category_id])
+		@product.save
+
+
+		redirect_to categories_path
+
+		
+	end
+
+	def destroy
+
+		@product = Product.find(params[:id])
+		@product.destroy
+
+		redirect_to @category
+
+
+		
+	end
+
+	private
+
+	def product_params
+		
+      params.require(:product).permit(:name, :price)
+    
+	end
+
+	def set_category
+        @category = Category.find(params[:category_id])
+    end
+
+end
